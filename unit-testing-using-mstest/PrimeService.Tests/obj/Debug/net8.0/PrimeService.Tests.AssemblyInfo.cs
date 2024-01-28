4fc4948188b4901f5a113827e26174c730f2a85e
Andrew Stanton <mail@andrewstanton.tech>
2024-01-28 18:59:42 +0000

    Changes when running test (rebuild)

--- unit-testing-using-mstest/PrimeService.Tests/obj/Debug/net8.0/PrimeService.Tests.AssemblyInfo.cs
+++ unit-testing-using-mstest/PrimeService.Tests/obj/Debug/net8.0/PrimeService.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fcca15d7cecd52186aeed0018d8f9e4b8f3e7fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3120a3002a39badfe91b38ecc35d9fee12234f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
