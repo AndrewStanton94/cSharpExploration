fe8fbfb920e3557db1779e08d4fd8680ee0b6f3b
Andrew Stanton <mail@andrewstanton.tech>
2024-01-28 21:52:40 +0000

    Tried given example in Chrome and Firefox

--- selenium/Selenium.Tests/UnitTest1.cs
+++ selenium/Selenium.Tests/UnitTest1.cs
@@ -1,3 +1,8 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
 namespace Selenium.Tests;
 
 [TestClass]
@@ -8,5 +13,25 @@ public class UnitTest1
     {
         Console.WriteLine("The test begins");
         Assert.AreEqual(true, true);
+
+        // IWebDriver driver = new ChromeDriver();
+        IWebDriver driver = new FirefoxDriver();
+
+        driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+        var title = driver.Title;
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+        var textBox = driver.FindElement(By.Name("my-text"));
+        var submitButton = driver.FindElement(By.TagName("button"));
+
+        textBox.SendKeys("Selenium");
+        submitButton.Click();
+
+        var message = driver.FindElement(By.Id("message"));
+        var value = message.Text;
+
+        // driver.Quit();
     }
 }
