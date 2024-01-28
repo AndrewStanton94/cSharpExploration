8c3120a3002a39badfe91b38ecc35d9fee12234f
Andrew Stanton <mail@andrewstanton.tech>
2024-01-28 18:40:53 +0000

    Make the test pass

--- unit-testing-using-mstest/PrimeService/PrimeService.cs
+++ unit-testing-using-mstest/PrimeService/PrimeService.cs
@@ -2,11 +2,15 @@
 
 namespace Prime.Services
 {
-    public class PrimeService
-    {
-        public bool IsPrime(int candidate)
-        {
-            throw new NotImplementedException("Please create a test first.");
-        }
-    }
+	public class PrimeService
+	{
+		public bool IsPrime(int candidate)
+		{
+			if (candidate == 1)
+			{
+				return false;
+			}
+			throw new NotImplementedException("Please create a test first.");
+		}
+	}
 }
